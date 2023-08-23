67af065b58cda9f6806aae293845ce94ee80a458
nyvan80076@gmail.com <nyvan80076@gmail.com>
2023-08-24 00:00:44 +0300

    first commit

---
+++ src/sections/PopularProducts.jsx
@@ -0,0 +1,23 @@
+
+import { products } from "../constants"
+import PopularProductCard from '../components/PopularProductCard'
+const PopularProducts = () => {
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold "> Our <span className="text-coral-red">Popular</span> Products</h2>
+        <p className="lg:max-w-lg font-montserrat text-slate-gray">
+          Experirnce top-notch quality snd sttyle with out sought-after selections. Discover a world of comfort, Design, and value.
+        </p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product)=>(
+          <PopularProductCard key={products.name} {...product} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default PopularProducts
