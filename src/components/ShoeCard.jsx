8ba5f374cb4e7337acfe2a1e926518933cd38d40
nyvan80076@gmail.com <nyvan80076@gmail.com>
2023-08-24 01:28:43 +0300

    making it more responsive

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,17 +1,17 @@
 
 
-const ShoeCard = ({imgUrl, changeBigShoeImage, bigShoeImg}) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
   const  handleClick = () => {
-    if(bigShoeImg !== imgUrl.bigShoe) {
-      changeBigShoeImage(imgUrl.bigShoe)
+    if(bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe)
     }
   }
   
   return (
-    <div className={`border-2 rounded-xl ${bigShoeImg === imgUrl.bigShoe?'border-coral-red': 'border-transparent'} cursor-pointer max-sm:flex-1 `}   onClick={handleClick}>
+    <div className={`border-2 rounded-xl ${bigShoeImg === imgURL.bigShoe?'border-coral-red': 'border-transparent'} cursor-pointer max-sm:flex-1 `}   onClick={handleClick}>
         <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
           <img 
-           src={imgUrl.thumbnail}
+           src={imgURL.thumbnail}
            alt="shoes collection" 
            width={127}
            height={103}
